6874095fd48b365ed091b43db436951ecf31ebeb
Fadiil <fadilthoriq201@gmail.com>
2023-06-26 14:20:19 +0700

    revisi title home

--- src/components/cta.jsx
+++ src/components/cta.jsx
@@ -26,7 +26,8 @@ export default function Cta() {
               strings: [
                 "Innovate!",
                 "Inspire!",
-                "Improve!"
+                "Improve!",
+                "Are Excodian!"
               ],
             }}
           />
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -2,18 +2,24 @@ import 'tailwindcss/tailwind.css';
 import Navbar from '@/components/navbar.jsx';
 import Cta from '@/components/cta.jsx';
 import Footer from '@/components/footer.jsx';
+import Head from 'next/head';
 
 function Home() {
   return (
     <section>
+      <Head>
+        <link rel="icon" href="/logo-exdecode-bgblue.png" />
+        <title>Home</title>
+      </Head>
       <div className='bg-gray-900'>
+
 
         <Navbar />
         <Cta />
 
       </div>
       <Footer />
-      
+
 
     </section>
 
