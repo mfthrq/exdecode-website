7cd751aa0334368029f3c1e9350049507a718c9a
Fadiil <fadilthoriq201@gmail.com>
2023-06-27 15:51:27 +0700

    contact done-1

--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -1,17 +1,24 @@
 "use client";
 
+import React from 'react';
 import 'tailwindcss/tailwind.css';
 import Navbar from '@/components/navbar';
 import Footer from '@/components/footer';
 import { motion } from "framer-motion";
 import { useState, useEffect } from 'react';
+import { useForm, ValidationError } from '@formspree/react';
 import countriesData from '@/components/codePhone.json';
 import Head from 'next/head';
 
 export default function Contact() {
+    const [state, handleSubmit] = useForm("mpzgarqy");
     const [phoneNumber, setPhoneNumber] = useState('');
     const [selectedCountry, setSelectedCountry] = useState('');
     const [countries, setCountries] = useState([]);
+
+    if (state.succeeded) {
+        return <p>Thanks for joining!</p>;
+    }
 
     useEffect(() => {
         // Mengatur data negara dari JSON ke dalam state countries
@@ -53,13 +60,14 @@ export default function Contact() {
                     <p class="mb-16 text-center text-white sm:text-xl text-justify">Got a problem on the server? Want to give feedback about the server? Or want to ask about our community partnership? Let us know!</p>
                 </motion.div>
 
-                <form action="https://formsubmit.co/exdecodeofficial@gmail.com" method="POST" class="-mt-8 space-y-8">
+                <form action="https://formspree.io/f/mpzgarqy"
+                    method="POST" class="-mt-8 space-y-8">
                     <motion.div
                         initial={{ opacity: 0, y: -80 }}
                         animate={{ opacity: 1, y: 0 }}
                         transition={{ duration: 1.5 }}>
-                        <label for="email" class="block mb-2 text-sm font-medium text-white">Your Email</label>
-                        <input type="email" id="email" class="block h-12 shadow-sm bg-gray-700 border text-white text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 border-gray-600 placeholder-gray-400" placeholder="excodian@gmail.com" required />
+                        <label htmlFor="email" class="block mb-2 text-sm font-medium text-white">Your Email</label>
+                        <input name="email" type="email" id="email" class="block h-12 shadow-sm bg-gray-700 border text-white text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 border-gray-600 placeholder-gray-400" placeholder="excodian@gmail.com" required />
                     </motion.div>
                     <motion.div
                         initial={{ opacity: 0, y: -80 }}
@@ -67,7 +75,7 @@ export default function Contact() {
                         transition={{ duration: 2 }}
                         className='w-full'
                     >
-                        <label for="phone" class="block mb-2 text-sm font-medium text-white">Phone Number</label>
+                        <label htmlFor="phone" class="block mb-2 text-sm font-medium text-white">Phone Number</label>
                         <div class='flex w-full'>
                             <select id="country" value={selectedCountry} onChange={handleCountryChange} class="flex-none h-12 w-24 p-3 text-sm text-white bg-gray-700 rounded-l-lg border shadow-sm focus:ring-primary-500 focus:border-primary-500 border-gray-600 placeholder-gray-400" required>
                                 {countriesData.map((country) => (
@@ -79,6 +87,7 @@ export default function Contact() {
                             <input
                                 type="text"
                                 id="phoneNumber"
+                                name="phoneNumber"
                                 value={phoneNumber}
                                 onChange={handlePhoneNumberChange}
                                 class="block flex-1 w-full rounded-r-lg h-12 shadow-sm bg-gray-700 border text-white text-sm focus:ring-primary-500 focus:border-primary-500 w-full p-2.5 border-gray-600 placeholder-gray-400" placeholder="123-456-78" required
@@ -90,16 +99,16 @@ export default function Contact() {
                         initial={{ opacity: 0, y: -80 }}
                         animate={{ opacity: 1, y: 0 }}
                         transition={{ duration: 2.5 }}>
-                        <label for="subject" class="block mb-2 text-sm font-medium text-white">Subject</label>
-                        <input type="text" id="subject" class="block h-12 shadow-sm bg-gray-700 border text-white text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 border-gray-600 placeholder-gray-400" placeholder="Let us know how we can help you" required />
+                        <label htmlFor="subject" class="block mb-2 text-sm font-medium text-white">Subject</label>
+                        <input name="subject" type="text" id="subject" class="block h-12 shadow-sm bg-gray-700 border text-white text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 border-gray-600 placeholder-gray-400" placeholder="Let us know how we can help you" required />
                     </motion.div>
                     <motion.div
                         initial={{ opacity: 0, y: -80 }}
                         animate={{ opacity: 1, y: 0 }}
                         transition={{ duration: 3 }}
                         class="sm:col-span-2">
-                        <label for="message" class="block mb-2 text-sm font-medium text-white">Your Message</label>
-                        <textarea id="message" rows="6" class="block h-32 shadow-sm bg-gray-700 border text-white text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 border-gray-600 placeholder-gray-400" placeholder="Leave a comment" required></textarea>
+                        <label htmlFor="message" class="block mb-2 text-sm font-medium text-white">Your Message</label>
+                        <textarea name="message" id="message" rows="6" class="block h-32 shadow-sm bg-gray-700 border text-white text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 border-gray-600 placeholder-gray-400" placeholder="Leave a comment" required></textarea>
                     </motion.div>
                     <motion.button
                         initial={{ opacity: 0, y: -80 }}
@@ -107,6 +116,7 @@ export default function Contact() {
                         transition={{ duration: 3.5 }}
                         type="submit"
                         className="flex items-center justify-center w-28 h-12 mb-8 inline-flex cursor-pointer text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center mr-3 md:mr-0 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                        disabled={state.submitting}
                     >
                         Submit
                     </motion.button>
